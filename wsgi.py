79d47d514698d4b8075b02a0bbbc8320254c5ca8
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-12-23 23:29:28 +1100

    Initial commit

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+# Set the path
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from application import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
