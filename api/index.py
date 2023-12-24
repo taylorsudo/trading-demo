bccf2e6f9aaf4feec2eac2dc9d733983c63eccc7
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-12-24 19:08:56 +1100

    Refactor helpers function call

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import apology, login_required, lookup, usd
+from api.helpers import apology, login_required, lookup, usd
 from datetime import datetime
 
 # Configure application
