ed4aeac7c033b85e0988790649d044d3929837a0
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-12-25 00:09:25 +1100

    Import Redis

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 import os
+import redis
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
@@ -6,7 +7,6 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from .helpers import apology, login_required, lookup, usd
 from datetime import datetime
-from redis import Redis
 
 # Configure application
 app = Flask(__name__)
