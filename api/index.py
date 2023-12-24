2d1698a36da6f28b2169c4554c4c3527bf1e3f88
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-12-24 22:44:27 +1100

    Fix typo

--- api/index.py
+++ api/index.py
@@ -29,8 +29,8 @@ Session(app)
 
 # Get environment variables for Postgres
 postgres_url = os.getenv("POSTGRES_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
+if postgres_url.startswith("postgres://"):
+    postgres_url = postgres_url.replace("postgres://", "postgresql://")
 db = SQL(postgres_url)
 
 
